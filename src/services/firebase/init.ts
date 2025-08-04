9bc089782f268bbb1a8cc6274c613ad02c1b070c
alina <berens.alina019@gmail.com>
2025-08-04 05:02:55 +0300

    native

--- src/services/firebase/init.ts
+++ src/services/firebase/init.ts
@@ -1,13 +1,11 @@
-// src/services/firebase/init.ts
-
-// Нативная инициализация RNFirebase (App Registry под капотом)
 import '@react-native-firebase/app';
-
-// Экспортируем «инстансы» модулей
 import auth from '@react-native-firebase/auth';
-import { default as db, default as firestore } from '@react-native-firebase/firestore';
+import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
 
+// Экспортируем готовые инстансы
+export { auth };
+export const db = firestore;
+export { storage };
 
-export { auth, db, firestore, storage };
 
